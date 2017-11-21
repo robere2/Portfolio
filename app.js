576b0d8fa62291a64958321ee6773b9ac9aa967d
bugfroggy <bugfroggy@gmail.com>
2017-11-21 15:39:15 -0500

    Worked on recaptcha for email

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var sassMiddleware = require('node-sass-middleware');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const sassMiddleware = require('node-sass-middleware');
 
-var index = require('./routes/index');
+let index = require('./routes/index');
+let captcha = require('./routes/captcha');
 
-var app = express();
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,24 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Defining our pages
 app.use('/', index);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use('/', captcha);
 
 module.exports = app;
 
