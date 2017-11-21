27b39a0371854afa0e5e6db2790c419724ede527
bugfroggy <bugfroggy@gmail.com>
2017-11-21 12:17:05 -0500

    Initial commit

---
+++ public/fa/js/packs/custom.js
@@ -0,0 +1,89 @@
+(function () {
+'use strict';
+
+var _WINDOW = {};
+try {
+  if (typeof window !== 'undefined') _WINDOW = window;
+  
+} catch (e) {}
+
+var _ref = _WINDOW.navigator || {};
+var _ref$userAgent = _ref.userAgent;
+var userAgent = _ref$userAgent === undefined ? '' : _ref$userAgent;
+
+var WINDOW = _WINDOW;
+
+
+
+
+var IS_IE = ~userAgent.indexOf('MSIE') || ~userAgent.indexOf('Trident/');
+
+var NAMESPACE_IDENTIFIER = '___FONT_AWESOME___';
+
+
+
+
+
+
+
+
+
+var oneToTen = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var oneToTwenty = oneToTen.concat([11, 12, 13, 14, 15, 16, 17, 18, 19, 20]);
+
+
+
+var RESERVED_CLASSES = ['xs', 'sm', 'lg', 'fw', 'ul', 'li', 'border', 'pull-left', 'pull-right', 'spin', 'pulse', 'rotate-90', 'rotate-180', 'rotate-270', 'flip-horizontal', 'flip-vertical', 'stack', 'stack-1x', 'stack-2x', 'inverse', 'layers', 'layers-text', 'layers-counter'].concat(oneToTen.map(function (n) {
+  return n + 'x';
+})).concat(oneToTwenty.map(function (n) {
+  return 'w-' + n;
+}));
+
+function bunker(fn) {
+  try {
+    fn();
+  } catch (e) {
+    
+  }
+}
+
+var icons$1 = {
+  "hypixel": [230, 320, [], "f96e", "M42,244l-13,6s32,78,63,92V175h12V290s41-10,63-63l-11-5V103s2-66,31-93c0,0-36-11-82,12l1,104v6H93L92,25S36,47,14,74c0,0,16,4,27,18Z"],
+};
+
+var w = WINDOW || {};
+
+if (!w[NAMESPACE_IDENTIFIER]) w[NAMESPACE_IDENTIFIER] = {};
+if (!w[NAMESPACE_IDENTIFIER].packs) w[NAMESPACE_IDENTIFIER].packs = {};
+if (!w[NAMESPACE_IDENTIFIER].hooks) w[NAMESPACE_IDENTIFIER].hooks = {};
+if (!w[NAMESPACE_IDENTIFIER].shims) w[NAMESPACE_IDENTIFIER].shims = [];
+
+var namespace = w[NAMESPACE_IDENTIFIER];
+
+var _extends = Object.assign || function (target) {
+  for (var i = 1; i < arguments.length; i++) {
+    var source = arguments[i];
+
+    for (var key in source) {
+      if (Object.prototype.hasOwnProperty.call(source, key)) {
+        target[key] = source[key];
+      }
+    }
+  }
+
+  return target;
+};
+
+function define(prefix) {
+  if (typeof namespace.hooks.addPack === 'function') {
+    namespace.hooks.addPack(prefix, icons$1);
+  } else {
+    namespace.packs[prefix] = _extends({}, namespace.packs[prefix] || {}, icons$1);
+  }
+}
+
+bunker(function () {
+  define('fac');
+});
+
+}());
