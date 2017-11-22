0f3fc4874b2645b8439c8eda9af7fb04e45f928b
bugfroggy <bugfroggy@gmail.com>
2017-11-22 10:12:30 -0500

    Changed some responsive CSS, Commented out debug logging

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ let app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+// Uncomment for debug
+// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
