0637cb7671fb54dbe0b9db920c098dd56b63b14d
Erik Roberts <erik@bugg.co>
2018-12-10 23:40:31 -0500

    * Replaced "About" section with "About Me" timeline * Added new timeline functionality

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home | bugg.co' });
+  // Calculate age
+  let born = 936921600000;
+  let age = new Date().getTime() - born;
+  let msPerYear = 31536000000;
+
+  res.render('index', { title: 'Home | bugg.co', age: Math.floor(age / msPerYear)});
   next();
 });
 
