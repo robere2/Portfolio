d5675c02dac5ab43ae469442bfb79d115904959e
Erik Roberts <bugfroggy@gmail.com>
2022-05-24 22:25:25 -0400

    update linkedin link

--- components/SelfIntro/SelfIntro.js
+++ components/SelfIntro/SelfIntro.js
@@ -26,7 +26,7 @@ class SelfIntro extends HTMLElement {
 	        </div>
 	        <div class="profiles">
                 <a class="profile-btn" href="https://github.com/robere2" aria-label="GitHub"><i class="fab fa-github"></i></a>
-                <a class="profile-btn" href="https://github.com/robere2" aria-label="LinkedIn"><i class="fab fa-linkedin"></i></a>
+                <a class="profile-btn" href="https://www.linkedin.com/in/erik-roberts-5664171a1/" aria-label="LinkedIn"><i class="fab fa-linkedin"></i></a>
                 <a class="profile-btn" href="https://www.npmjs.com/~bugfroggy" aria-label="NPM"><i class="fab fa-npm"></i></a>
 			</div>
 		`;
